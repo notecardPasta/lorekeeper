88d1b175bca8bf18e2a92448795d8f25ec995623
notecardPasta <notecardpasta@gmail.com>
2026-04-25 21:55:33 -0400

    fixed table typo

--- database/migrations/2026_04_23_223236_create__reward_choice_groups_table.php
+++ database/migrations/2026_04_23_223236_create__reward_choice_groups_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('_reward_choice_groups', function (Blueprint $table) {
+        Schema::create('reward_choice_groups', function (Blueprint $table) {
             $table->id();
             $table->string('name'); 
             $table->boolean('is_character')->default(1);                         
